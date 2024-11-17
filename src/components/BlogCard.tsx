f7a75abecc0a11a270ea570f99951c0313f29574
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-17 20:59:40 +0500

    Update BlogCard.tsx

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -9,15 +9,15 @@ interface propsType {
 
 const BlogCard = ({img, title, date}: propsType) => {
   return (
-    <div className='space-y-1'>
+    <div className='pl-6'>
       <Image
        className='rounded-lg hover:scale-105 transition-transform'
        src={img}
        alt="post"
-       width={300}
-       height={300}
+       width={400}
+       height={400}
        />
-       <div className='text-orange-500 text-center font-medium'>
+       <div className='text-orange-500 text-[10px] text-center font-medium'>
        <span>{date}</span>
        </div>
        <h3 className='font-thin font-serif text-center text-[20px] hover:text-orange-600'>{title}</h3>
