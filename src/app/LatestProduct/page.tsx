41df108af95989b19262a449d7a28367c0361577
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-17 20:09:24 +0500

    Update page.tsx

--- src/app/LatestProduct/page.tsx
+++ src/app/LatestProduct/page.tsx
@@ -5,13 +5,13 @@ const ShopNowPage = () => {
   return (
     <div>
      <div> <h1 className='text-[30px] text-center text-red-900 font-serif font-bold pt-4'>Latest Product</h1></div>
-      <div className='flex-col flex sm:flex-row m-3 sm:mr-8 gap-1'>
-      <Image src="/Image/camerabottlecolors.jpg" alt="camera bottle" width={350} height={3500} 
-      className="mt-16 ml-11" />
-      <Image src="/Image/camerabottleyg.jpg" alt="camera bottle" width={350} height={350} 
-      className="mt-16 ml-11"/>
+      <div flex-col flex sm:flex-row m-3 sm:mr-8 pl-10 gap-1 w-full h-auto'>
+      <Image src="/Image/camerabottlecolors.jpg" alt="camera bottle" width={300} height={300} 
+      className="mt-16 ml-10 mr-10 border border-black hover:translate-x-2" />
+      <Image src="/Image/camerabottleyg.jpg" alt="camera bottle" width={300} height={300} 
+      className="mt-16 ml-10 mr-10 border border-black hover:translate-x-2"/>
       <Image src="/Image/camerabottlepg.jpg" alt="camera bottle" width={350} height={350} 
-      className="mt-16 ml-11"/>
+      className="mt-16 ml-10 mr-10 border border-black hover:translate-x-2"/>
       </div>
       </div>
     )
